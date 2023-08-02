9680ab3ca4363911449214871e7f664b3bd8717a
Rahul Pachar <rahulpachar0786@gmail.com>
2023-08-02 17:58:49 +0530

    hero

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className='flex justify-between items-center h-20 px-4'>
+        <div className='flex justify-between items-center h-20 px-4 absolute text-white z-10 w-full'>
             <div>
                 <h1 onClick={handleNav} className={logo ? "hidden" : "block"}>Beaches</h1>
             </div>
@@ -28,14 +28,14 @@ const Navbar = () => {
                 <li>Book</li>
             </ul>
             <div className='hidden md:flex'>
-                <BiSearch className='mr-2' size={20} />
+                <BiSearch className='' size={20} />
                 <BsPerson size={20} />
             </div>
             <div onClick={handleNav} className='md:hidden z-10'>
                 {nav ? <AiOutlineClose className='text-black' size={20}/> : <HiOutlineMenuAlt4 size={20} />}
                 
             </div>
-            <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90  px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+            <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full text-black bg-gray-100/90  px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
                 <ul>
                     <h1>Beaches</h1>
                     <li className='border-b'>Home</li>
