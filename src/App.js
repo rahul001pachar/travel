6905522a6368cb5d50b9b873bd4563e6bf593f21
Rahul Pachar <rahulpachar0786@gmail.com>
2023-08-02 22:48:24 +0530

    destinations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
+import Destinations from './Components/Destinations';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <Destinations/>
     </>
   );
 }
---
+++ src/Components/Destinations.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import BoraBora from "../Assets/borabora.jpg"
+import BoraBora2 from "../Assets/borabora2.jpg"
+import Maldives from "../Assets/maldives.jpg"
+import Maldives2 from "../Assets/maldives2.jpg"
+import KeyWest from "../Assets/keywest.jpg"
+
+
+const Destinations = () => {
+    return (
+        <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
+            <h1 className=''>All-Inclusive Resorts</h1>
+            <p className='py-4'>On the Caribbean's Best Beaches</p>
+            <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'> 
+            <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={BoraBora} alt="/" />
+            <img className='w-full h-full object-cover' src={BoraBora2} alt="/" />
+            <img className='w-full h-full object-cover' src={Maldives} alt="/" />
+            <img className='w-full h-full object-cover' src={Maldives2} alt="/" />
+            <img className='w-full h-full object-cover' src={KeyWest} alt="/" />
+        </div>
+        </div>
+    )
+}
+
+export default Destinations 
