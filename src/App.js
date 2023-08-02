28d1761afa1ffa70ad63c85d291d354ab25283a9
Rahul Pachar <rahulpachar0786@gmail.com>
2023-08-02 13:35:01 +0530

    comp. navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   );
 }
